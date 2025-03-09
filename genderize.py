7da84a7af909088763b49506b83006c29743693f
levivika <levivika35@mail.ru>
2025-03-10 00:06:06 +0300

    initial

--- genderize.py
+++ genderize.py
@@ -1,4 +1,10 @@
 import requests
+'''
+Дописать программу таким образом, чтобы в случае корректного имени выводилось сообщение
+в формате: Имя Elena относится к женскому полу с вероятностью 99%
+В случае некорректного имени выводилось сообщение Имя введено не корректно.
+'''
+
 
 
 
@@ -13,9 +19,30 @@ def parce_data(data: str):
     name = data.get('name')
     gender = data.get('gender')
     probability = data.get('probability')
-    print(name, gender, probability)
+    inf = (name, gender, probability)
+    return inf
+
+def data_to_format(inf):
+    name, gender, probability = inf
+    if name:
+        if gender == 'female':
+            gender = 'к женскому полу'
+        else:
+            gender = 'к мужскому полу'
+        if probability:
+            probability=int(probability*100)
+        text = f'Имя {name} относится {gender} с вероятностью {probability}%'
+    else:
+        text = 'Имя введено не корректно.'
+    return text
+
+
+
+
+
 
 
 name = input('Name: ')
 data = get_data(name)
-parce_data(data)
+inf = parce_data(data)
+print(data_to_format(inf))
